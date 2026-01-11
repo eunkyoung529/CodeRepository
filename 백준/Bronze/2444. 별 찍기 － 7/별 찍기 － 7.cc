3a3ed998963169b4cef7aaf363baa4a577794693
eunkyoung529 <eunkyoung4160@gmail.com>
2026-01-11 14:14:38 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, i, j, k;
+	cin >> n;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = n; j > i; j--)
+		{
+			cout << ' ';
+		}
+		for (k = 1; k <= i; k++)
+		{
+			cout << '*';
+		}
+		for (j = 1; j < i; j++)
+		{
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	for (i = 1; i < n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			cout << ' ';
+		}
+		for (k = n-1; k >= i; k--)
+		{
+			cout << '*';
+		}
+		for (j = n-1; j > i; j--)
+		{
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	
+	return 0;
+}
