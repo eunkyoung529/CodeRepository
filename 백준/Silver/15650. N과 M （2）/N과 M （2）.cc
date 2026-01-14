2dcfc5ca242746c19ccc3a1791cb8d59a0572e32
eunkyoung529 <eunkyoung4160@gmail.com>
2026-01-14 21:10:48 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int arr[10];
+
+void dfs(int k, int start)
+{
+	int i;
+	if (k == m)
+	{
+		for (i = 0; i < m; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << "\n";
+		return;
+	}
+	for (i = start; i <= n; i++)
+	{
+		arr[k] = i;
+		dfs(k + 1, i + 1);
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	dfs(0, 1);
+	return 0;
+}
