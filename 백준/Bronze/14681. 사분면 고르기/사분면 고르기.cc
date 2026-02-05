a36f529e5d54c3b8e877bb962d6f1c7491edce3a
eunkyoung529 <eunkyoung4160@gmail.com>
2026-02-05 23:32:28 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int x, y;
+
+int main()
+{
+	int i;
+	cin >> x >> y;
+	if (x > 0 && y > 0)
+	{
+		cout << "1" << "\n";
+	}
+	else if (x < 0 && y > 0)
+	{
+		cout << "2" << "\n";
+	}
+	else if (x < 0 && y < 0)
+	{
+		cout << "3" << "\n";
+	}
+	else if (x > 0 && y < 0)
+	{
+		cout << "4" << "\n";
+	}
+
+	return 0;
+}
